335ef881e36e1fa714ed3aec579125396babe3eb
Mohammed Ali <mohaali482@gmail.com>
2024-10-01 18:10:11 +0300

    feat: udpate layout

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,15 +1,19 @@
+import Footer from "@/components/footer";
 import NavBar from "@/components/ui/navbar";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div>
-            <header>
-                <NavBar />
-            </header>
-            <main className="px-8">
+        <div className="min-h-[calc(100dvh)] bg-repeat-y"
+            style={{
+                backgroundImage: 'url(/images/background.jpg)',
+                backgroundColor: "#2F1E23"
+            }}>
+            <NavBar />
+            <main className="px-4 h-full lg:px-16">
                 <Outlet />
             </main>
+            <Footer />
         </div>
     )
 }
