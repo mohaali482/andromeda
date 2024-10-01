cbd41894d88e8a492f13c543c03341af4e85e901
Mohammed Ali <mohaali482@gmail.com>
2024-10-01 17:41:00 +0300

    feat: add footer component

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,15 @@
+import { GitHubLogoIcon, PersonIcon } from "@radix-ui/react-icons";
+import { BriefcaseBusiness, Github, Linkedin } from "lucide-react";
+
+export default function Footer() {
+    return (
+        <footer className="bg-background/95 text-foreground p-4 text-center">
+            <div className="flex justify-center pt-8">
+                <a href="https://mohammedali.vercel.app/" className="mx-2 size-10"><BriefcaseBusiness size={35} /></a>
+                <a href="https://www.linkedin.com/in/mohammed-ali-fenta/" className="mx-2"><Linkedin size={35} /></a>
+                <a href="https://github.com/mohaali482" className="mx-2"><Github size={35} /></a>
+            </div>
+            <p>From Mohammed Ali to the world.</p>
+        </footer>
+    )
+}
