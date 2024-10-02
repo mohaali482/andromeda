e00387e1aa4004e5c931be6d87ad33ac7cb346a6
Mohammed Ali <mohaali482@gmail.com>
2024-10-02 07:17:19 +0300

    feat: add a route for apod handler

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,30 @@
+import cors from "cors";
 import express from "express";
+import { Redis } from "@upstash/redis";
+import { apiKey, nasaURL, port, redisToken, redisURL } from "./config";
+import logger from "./middlewares/logger";
+import apodHandler from "./controllers/apod";
+
+const redis = new Redis({
+  url: redisURL,
+  token: redisToken,
+});
+
+const config = {
+  apiKey,
+  nasaURL,
+  port: port,
+};
 
 const app = express();
-const port = 8000;
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// CORS middleware
+app.use(cors());
+// Logger middleware
+app.use(logger());
+
+app.get("/apod", apodHandler(redis, config));
 
 app.listen(port, () => {
   return console.log(`http://localhost:${port}`);
