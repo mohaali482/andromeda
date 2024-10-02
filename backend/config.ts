c605ef988cadbd5a7b31702fb7b465916eaab02c
Mohammed Ali <mohaali482@gmail.com>
2024-10-02 07:16:07 +0300

    feat: add config file

---
+++ backend/config.ts
@@ -0,0 +1,5 @@
+export const port = process.env.PORT || "8000";
+export const apiKey = process.env.API_KEY;
+export const nasaURL = "https://api.nasa.gov";
+export const redisURL = process.env.UPSTASH_URL;
+export const redisToken = process.env.REDIS_TOKEN;
