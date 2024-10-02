0ec983bc6ed2313f3ff571bf466d399a619bded6
Mohammed Ali <mohaali482@gmail.com>
2024-10-02 07:15:55 +0300

    feat: add backend types

---
+++ backend/types.ts
@@ -0,0 +1,15 @@
+export type ApodData = {
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+};
+
+export type Config = {
+  apiKey: string;
+  nasaURL: string;
+  port: string;
+};
