5b16eb85784a61d12654ddc5207eb140006cf02d
Mohammed Ali <mohaali482@gmail.com>
2024-10-04 21:06:17 +0300

    feat: add mars rover photos page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Layout from "./pages/layout"
 import Home from "./pages/home"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import MarsRoverPhotos from "./pages/mars-rover-photos"
 
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/mars-rover-photos" element={<MarsRoverPhotos />} />
+          </Route>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
