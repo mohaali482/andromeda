a43a934a351d48fdc70d447cd1b83d6f5ad70cce
Mohammed Ali <mohaali482@gmail.com>
2024-10-04 21:01:23 +0300

    feat: add mars rover photos types

---
+++ frontend/src/types/mars-rover-photos.ts
@@ -0,0 +1,32 @@
+export type Rover = {
+  name: string;
+  image: string;
+  alt: string;
+  description: string;
+  moreInfo: string;
+  cameras: string[];
+};
+
+type MarsRoverPhotosData = {
+  id: number;
+  sol: number;
+  camera: {
+    id: number;
+    name: string;
+    rover_id: number;
+    full_name: string;
+  };
+  img_src: string;
+  earth_date: string;
+  rover: {
+    id: number;
+    name: string;
+    landing_date: string;
+    launch_date: string;
+    status: string;
+  };
+};
+
+export type MarsRoverResponse = {
+  photos: MarsRoverPhotosData[];
+};
