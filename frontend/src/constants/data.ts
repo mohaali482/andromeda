bc2dbcd3276650d62a2a75982db30fcff088c37a
Mohammed Ali <mohaali482@gmail.com>
2024-10-04 21:02:39 +0300

    feat: add list of rovers

---
+++ frontend/src/constants/data.ts
@@ -0,0 +1,38 @@
+export const RoverList = [
+  {
+    name: "curiosity",
+    image: "/images/curiosity.jpg",
+    alt: "Curiosity Rover",
+    description:
+      "Curiosity, NASA's largest rover launched in 2011, explores Mars to determine if it once had conditions suitable for microbial life, finding evidence of past habitable environments.",
+    moreInfo: "https://science.nasa.gov/mission/msl-curiosity/",
+    cameras: [
+      "all",
+      "fhaz",
+      "rhaz",
+      "mast",
+      "chemcam",
+      "mahli",
+      "mardi",
+      "navcam",
+    ],
+  },
+  {
+    name: "opportunity",
+    image: "/images/opportunity.jpg",
+    alt: "Opportunity Rover",
+    description:
+      "Opportunity, one of NASA's most successful Mars missions, landed in 2004 and operated for nearly 15 years, setting records and making key discoveries on the planet.",
+    moreInfo: "https://science.nasa.gov/mission/mer-opportunity/",
+    cameras: ["all", "fhaz", "rhaz", "navcam", "pancam", "minites"],
+  },
+  {
+    name: "spirit",
+    image: "/images/spirit.jpg",
+    alt: "Spirit Rover",
+    description:
+      "Spirit, one of NASA's twin rovers that landed on Mars in January 2004, searched for water history and far outlasted its 90-day mission, revealing that Mars was once much wetter and providing insights into Martian wind.",
+    moreInfo: "https://science.nasa.gov/mission/mer-spirit",
+    cameras: ["all", "fhaz", "rhaz", "navcam", "pancam", "minites"],
+  },
+];
