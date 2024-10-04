83e329e21d2c7aa05d5fc92d72495e61e25a0443
Mohammed Ali <mohaali482@gmail.com>
2024-10-04 21:03:17 +0300

    feat: add nasa api endpoint

--- frontend/src/api/nasa.ts
+++ frontend/src/api/nasa.ts
@@ -1,3 +1,24 @@
 const BACKEND_URL = process.env.BACKEND_URL;
 
 export const APOD_URL = `${BACKEND_URL}/apod`;
+const MARS_ROVER_PHOTOS = `${BACKEND_URL}/mars/:rover/photos`;
+
+export function buildMarsRoverPhotosURL(
+  page: number,
+  rover: string,
+  camera: string,
+  sol: number,
+  earthDate: string
+) {
+  const params = new URLSearchParams();
+  params.append("page", page.toString());
+  if (camera && camera !== "all") {
+    params.append("camera", camera);
+  }
+  if (earthDate) {
+    params.append("earth_date", earthDate);
+  } else {
+    params.append("sol", sol.toString());
+  }
+  return `${MARS_ROVER_PHOTOS.replace(":rover", rover)}?${params.toString()}`;
+}
